463bfac226e7bfb92be287bd54df53f1f4384869
rsbruno <rsbruno.cdc@gmail.com>
2023-01-28 18:14:10 -0300

    config:initial configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+import 'normalize.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <section style={{ width: '100%', height: '100vh' }}>
+    <Component {...pageProps} />
+  </section>
 }
---
+++ src/pages/certificates.tsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/certificates.module.scss'
+
+export default function CertificatesPage() {
+    return (
+        <div className={styles.container}>
+            hello
+        </div>
+    )
+
+}
